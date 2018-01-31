e7843b7184e93c577a4a259e92279d0b6adfad87
Nicoer <saber00sword@gmail.com>
2018-02-01 00:09:13 +0800

    网络版外部排序

--- cmd/externalsort/externalsort.go
+++ cmd/externalsort/externalsort.go
@@ -5,38 +5,39 @@ import (
 	"test/newProject/pipeline"
 	"bufio"
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	p := createPipeline("large.in",800000000,4)
-	writeToFile(p,"large.out")
+	p := createPipeline("large.in", 800000000, 4)
+	writeToFile(p, "large.out")
 	printFile("large.out")
 }
 func printFile(filename string) {
-	file,err:=os.Open(filename)
+	file, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-	p:=pipeline.ReaderSource(file,-1)
-	count:=0
-	for v:=range p{
+	p := pipeline.ReaderSource(file, -1)
+	count := 0
+	for v := range p {
 		fmt.Println(v)
 		count ++
-		if count>=100 {
+		if count >= 100 {
 			break
 		}
 	}
 }
 func writeToFile(p <-chan int, filename string) {
-	file,err:=os.Create(filename)
-	if err !=nil{
+	file, err := os.Create(filename)
+	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 	writer := bufio.NewWriter(file)
 	defer writer.Flush()
-	pipeline.WriteSink(writer,p)
+	pipeline.WriteSink(writer, p)
 }
 func createPipeline(filename string, fileSize, chunkCount int) <-chan int {
 	chunkSize := fileSize / chunkCount
@@ -49,7 +50,29 @@ func createPipeline(filename string, fileSize, chunkCount int) <-chan int {
 		}
 		file.Seek(int64(i*chunkSize), 0)
 		source := pipeline.ReaderSource(bufio.NewReader(file), chunkSize)
-		sortResults=append(sortResults,pipeline.InMemSort(source))
+		sortResults = append(sortResults, pipeline.InMemSort(source))
+	}
+	return pipeline.MergeN(sortResults...)
+}
+
+func createNetworkPipeline(filename string, fileSize, chunkCount int) <-chan int {
+	chunkSize := fileSize / chunkCount
+	pipeline.Init()
+	sortAddr := []string{}
+	for i := 0; i < chunkCount; i++ {
+		file, err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+		file.Seek(int64(i*chunkSize), 0)
+		source := pipeline.ReaderSource(bufio.NewReader(file), chunkSize)
+		addr := ":" + strconv.Itoa(7000+i)
+		pipeline.NetworkSink(addr, pipeline.InMemSort(source))
+		sortAddr = append(sortAddr, addr)
+	}
+	sortResults := []<-chan int{}
+	for _,addr:=range sortAddr{
+		sortResults=append(sortResults,pipeline.NetworkSource(addr))
 	}
 	return pipeline.MergeN(sortResults...)
 }
